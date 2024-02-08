854babb38b3600beb14d9aa6ff7cebc9d7726c9b
Christoph Michelbach <michelbach94@gmail.com>
2024-02-08 19:19:51 +0100

    Implement retrieval, extraction, and execution of gitleaks.

--- src/secguro/secguro.go
+++ src/secguro/secguro.go
@@ -3,9 +3,29 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 )
 
+// TODO: turn into CLI parameter
+const directoryToScan = "/home/christoph/Development/Work/wallet/"
+
 func main() {
-	fmt.Println("tt")
+	err := downloadDependencies()
+	if err != nil {
+		panic(err)
+	}
+
+	err = extractDependencies()
+	if err != nil {
+		panic(err)
+	}
+
+	cmd := exec.Command(dependenciesDir+"/gitleaks/gitleaks", "detect", "-v")
+	cmd.Dir = directoryToScan
+	// Ignore error because this is expected to deliver an exict code not equal to 0.
+	out, _ := cmd.Output()
+
+	fmt.Println(string(out[:]))
+
 	os.Exit(0)
 }
