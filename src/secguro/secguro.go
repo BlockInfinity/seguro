937bf2e5325cb71aa84a4ab066aab90c609ca9cd
Christoph Michelbach <michelbach94@gmail.com>
2024-02-09 20:59:17 +0100

    Implement CLI argument and flag parsing.

--- src/secguro/secguro.go
+++ src/secguro/secguro.go
@@ -3,14 +3,65 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
+
+	"github.com/urfave/cli/v2"
 )
 
 // const directoryToScan = "/home/christoph/Development/Work/wallet/"
 const directoryToScan = "."
 
 func main() {
+	var flagScanGitGistory string
+	var flagFormat string
+
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "scan-git-history",
+				Value:       "true",
+				Usage:       "true or false",
+				Destination: &flagScanGitGistory,
+			},
+			&cli.StringFlag{
+				Name:        "format",
+				Value:       "text",
+				Usage:       "text or json",
+				Destination: &flagFormat,
+			},
+		},
+		Action: func(cCtx *cli.Context) error {
+			name := ""
+			if cCtx.NArg() > 0 {
+				name = cCtx.Args().Get(0)
+			}
+
+			if name != "scan" {
+				log.Fatal("unsupported command")
+			}
+
+			if flagScanGitGistory != "true" && flagScanGitGistory != "false" {
+				log.Fatal("unsupported value for --scan-git-history")
+			}
+
+			if flagFormat != "text" && flagFormat != "json" {
+				log.Fatal("unsupported value for --format")
+			}
+
+			commandScan(flagScanGitGistory == "true", flagFormat == "json")
+
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func commandScan(scanGitHistory bool, printAsJson bool) {
 	err := downloadDependencies()
 	if err != nil {
 		panic(err)
