358089fad3941e43f8988395e720d7d8e40a488a
Christoph Michelbach <michelbach94@gmail.com>
2024-03-06 22:38:18 +0100

    Implement interactivity.

--- src/secguro/secguro.go
+++ src/secguro/secguro.go
@@ -11,81 +11,115 @@ import (
 
 var directoryToScan = "."
 
-func main() { //nolint: funlen
+func main() { //nolint: funlen, cyclop
 	var flagGitMode bool
 	var flagFormat string
 	var flagOutput string
 	var flagTolerance int
 	var flagDisabledDetectors []string
 
+	// TODO: decide whether to support the same flags for "scan"
+	// and "fix" (e.g. "tolerance" might not be useful for "fix").
+	flags := []cli.Flag{
+		&cli.BoolFlag{ //nolint: exhaustruct
+			Name:        "git",
+			Usage:       "set to scan git history and print commit information",
+			Destination: &flagGitMode,
+		},
+		&cli.StringFlag{ //nolint: exhaustruct
+			Name:        "format",
+			Value:       "text",
+			Usage:       "text or json",
+			Destination: &flagFormat,
+		},
+		&cli.StringFlag{ //nolint: exhaustruct
+			Name:        "output",
+			Aliases:     []string{"o"},
+			Value:       "",
+			Usage:       "path to output destination",
+			Destination: &flagOutput,
+		},
+		&cli.IntFlag{ //nolint: exhaustruct
+			Name:        "tolerance",
+			Value:       0,
+			Usage:       "number of findings to tolerate when choosing exit code",
+			Destination: &flagTolerance,
+		},
+		&cli.MultiStringFlag{
+			Target: &cli.StringSliceFlag{ //nolint: exhaustruct
+				Name:  "disabled-detectors",
+				Usage: "list of detectors to disable (semgrep,gitleaks,dependencycheck)",
+			},
+			Value:       []string{},
+			Destination: &flagDisabledDetectors,
+		},
+	}
+
+	action := func(cCtx *cli.Context) error {
+		if cCtx.NArg() > 0 {
+			directoryToScan = cCtx.Args().Get(0)
+		}
+
+		if cCtx.NArg() > 1 {
+			return errors.New("too many arguments")
+		}
+
+		if flagFormat != "text" && flagFormat != "json" {
+			return errors.New("unsupported value for --format")
+		}
+
+		if !arrayIncludes(flagDisabledDetectors, "dependencycheck") {
+			if os.Getenv(nvdApiKeyEnvVarName) == "" {
+				fmt.Printf("Disabling detector dependencycheck because "+
+					"environment variable %s is not set or is empty. "+
+					"You may apply for an API key at: "+
+					"https://nvd.nist.gov/developers/request-an-api-key\n", nvdApiKeyEnvVarName)
+
+				flagDisabledDetectors = append(flagDisabledDetectors, "dependencycheck")
+			}
+		}
+
+		printAsJson := flagFormat == "json"
+
+		switch cCtx.Command.Name {
+		case "scan":
+			{
+				err := commandScan(flagGitMode, flagDisabledDetectors,
+					printAsJson, flagOutput, flagTolerance)
+				if err != nil {
+					return err
+				}
+			}
+		case "fix":
+			{
+				err := commandFix(flagGitMode, flagDisabledDetectors,
+					printAsJson, flagOutput)
+				if err != nil {
+					return err
+				}
+			}
+		default:
+			{
+				return errors.New("unsupported command")
+			}
+		}
+
+		return nil
+	}
+
 	app := &cli.App{ //nolint: exhaustruct
 		Commands: []*cli.Command{
 			{
-				Name:  "scan",
-				Usage: "scan for problems",
-				Flags: []cli.Flag{
-					&cli.BoolFlag{ //nolint: exhaustruct
-						Name:        "git",
-						Usage:       "set to scan git history and print commit information",
-						Destination: &flagGitMode,
-					},
-					&cli.StringFlag{ //nolint: exhaustruct
-						Name:        "format",
-						Value:       "text",
-						Usage:       "text or json",
-						Destination: &flagFormat,
-					},
-					&cli.StringFlag{ //nolint: exhaustruct
-						Name:        "output",
-						Aliases:     []string{"o"},
-						Value:       "",
-						Usage:       "path to output destination",
-						Destination: &flagOutput,
-					},
-					&cli.IntFlag{ //nolint: exhaustruct
-						Name:        "tolerance",
-						Value:       0,
-						Usage:       "number of findings to tolerate when choosing exit code",
-						Destination: &flagTolerance,
-					},
-					&cli.MultiStringFlag{
-						Target: &cli.StringSliceFlag{ //nolint: exhaustruct
-							Name:  "disabled-detectors",
-							Usage: "list of detectors to disable (semgrep,gitleaks,dependencycheck)",
-						},
-						Value:       []string{},
-						Destination: &flagDisabledDetectors,
-					},
-				},
-				Action: func(cCtx *cli.Context) error {
-					if cCtx.NArg() > 0 {
-						directoryToScan = cCtx.Args().Get(0)
-					}
-
-					if cCtx.NArg() > 1 {
-						return errors.New("too many arguments")
-					}
-
-					if flagFormat != "text" && flagFormat != "json" {
-						return errors.New("unsupported value for --format")
-					}
-
-					if !arrayIncludes(flagDisabledDetectors, "dependencycheck") {
-						if os.Getenv(nvdApiKeyEnvVarName) == "" {
-							fmt.Printf("Disabling detector dependencycheck because "+
-								"environment variable %s is not set or is empty. "+
-								"You may apply for an API key at: "+
-								"https://nvd.nist.gov/developers/request-an-api-key\n", nvdApiKeyEnvVarName)
-
-							flagDisabledDetectors = append(flagDisabledDetectors, "dependencycheck")
-						}
-					}
-
-					err := commandScan(flagGitMode, flagDisabledDetectors,
-						flagFormat == "json", flagOutput, flagTolerance)
-
-					return err
-				},
+				Name:   "scan",
+				Usage:  "scan for problems",
+				Flags:  flags,
+				Action: action,
+			},
+			{
+				Name:   "fix",
+				Usage:  "scan for problems and then switch to an interactive mode to fix them",
+				Flags:  flags,
+				Action: action,
 			},
 		},
 		Action: func(cCtx *cli.Context) error {
