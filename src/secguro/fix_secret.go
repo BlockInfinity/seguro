8498690383c21a07b34c07fd0cba802b14dc6b7e
Christoph Michelbach <michelbach94@gmail.com>
2024-03-08 13:32:53 +0100

    Implement prompting for exact secret to be removed.

--- src/secguro/fix_secret.go
+++ src/secguro/fix_secret.go
@@ -1,10 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+)
 
 func fixSecret(unifiedFinding UnifiedFinding) error {
 	fmt.Println("would fix:")
 	fmt.Println(unifiedFinding)
+
+	// TODO: make line breaks dependent on the terminal's width
+	prompt := "Please specify the secret to remove from all git history. \n" +
+		"Note that we are not always able to determine the exact bounds of \n" +
+		"the secret, so it's important you specify the secret exactly."
+
+	secret := ""
+	for secret == "" {
+		var err error
+		secret, err = getTextInput(prompt, unifiedFinding.Match)
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Println("got:")
+	fmt.Println(secret)
 
 	return nil
 }
