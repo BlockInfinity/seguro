BlockInfinity/seguro

commit b87288256287e6ba564d149d3fc00eb696a378bd

Move logic for determining whether file exists to a re-usable function.

pkg/login/login.go
@@ -11,6 +11,7 @@ import (
 	"github.com/go-resty/resty/v2"
 	"github.com/secguro/secguro-cli/pkg/config"
 	"github.com/secguro/secguro-cli/pkg/types"
+	"github.com/secguro/secguro-cli/pkg/utils"
 )
 
 const deviceTokenFileName = "device_token"
@@ -140,20 +141,25 @@ func getDeviceToken() (string, error) {
 		return "", err
 	}
 
-	if _, err := os.Stat(pathSecguroConfigDir + "/" + deviceTokenFileName); err == nil {
-		authTokenBytes, err := os.ReadFile(pathSecguroConfigDir + "/" + deviceTokenFileName)
-		if err != nil {
-			return "", err
-		}
+	deviceTokenFilePath := pathSecguroConfigDir + "/" + deviceTokenFileName
 
-		authToken := string(authTokenBytes)
-
-		return authToken, nil
-	} else if errors.Is(err, os.ErrNotExist) {
-		return "", nil
-	} else {
+	doesFileExist, err := utils.DoesFileExist(deviceTokenFileName)
+	if err != nil {
 		return "", errors.New("cannot determine whether user is logged in")
 	}
+
+	if !doesFileExist {
+		return "", nil
+	}
+
+	authTokenBytes, err := os.ReadFile(deviceTokenFilePath)
+	if err != nil {
+		return "", err
+	}
+
+	authToken := string(authTokenBytes)
+
+	return authToken, nil
 }
 
 func ensureDirectoryExists(path string) error {

pkg/utils/files.go
@@ -1,0 +1,16 @@
+package utils
+
+import (
+	"errors"
+	"os"
+)
+
+func DoesFileExist(path string) (bool, error) {
+	if _, err := os.Stat(path); err == nil {
+		return true, nil
+	} else if errors.Is(err, os.ErrNotExist) {
+		return false, nil
+	} else {
+		return false, errors.New("cannot determine whether file exists")
+	}
+}

commit 245a8765432b39119c2c5942127a00e996a37fed

Fix bug that caused application to assume user is not logged in.
- bug introduced in b872882

pkg/login/login.go
@@ -143,7 +143,7 @@ func getDeviceToken() (string, error) {
 
 	deviceTokenFilePath := pathSecguroConfigDir + "/" + deviceTokenFileName
 
-	doesFileExist, err := utils.DoesFileExist(deviceTokenFileName)
+	doesFileExist, err := utils.DoesFileExist(deviceTokenFilePath)
 	if err != nil {
 		return "", errors.New("cannot determine whether user is logged in")
 	}